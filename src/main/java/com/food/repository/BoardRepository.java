adf1af4e9e519cbf6838fc96282418ebc566358a
임채성 <pan1129@naver.com>
2022-06-29 19:56:34 +0900

    텍스트 수정 Commit

--- src/main/java/com/food/repository/BoardRepository.java
+++ src/main/java/com/food/repository/BoardRepository.java
@@ -1,10 +1,7 @@
 package com.food.repository;
 
 
-import com.food.dto.BoardSearchDto;
 import com.food.entity.Board;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
